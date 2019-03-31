1d3eb9660cba2be7edd9f16d27506cb3dd9991bc
liaojuan <985434609@qq.com>
2019-03-31 22:53:38 +0800

    kotlin中listview的学习

--- app/src/main/java/com/example/pc/kotlin/ui/activity/ListViewActivity.kt
+++ app/src/main/java/com/example/pc/kotlin/ui/activity/ListViewActivity.kt
@@ -1,6 +1,7 @@
 package com.example.pc.kotlin.ui.activity
 
 import com.example.pc.kotlin.R
+import com.example.pc.kotlin.ui.adapter.ListViewAdapter
 import com.example.pc.kotlin.ui.base.BaseActivity
 import com.example.pc.kotlin.ui.contract.ListViewContract
 import com.example.pc.kotlin.ui.presenter.ListViewPresenter
@@ -8,6 +9,8 @@ import kotlinx.android.synthetic.main.activity_list_view.*
 
 class ListViewActivity : BaseActivity(), ListViewContract.View {
     private var listViewPresenter: ListViewPresenter ?= null
+    var dataList = ArrayList<String>()
+    var mAdapter: ListViewAdapter ?= null
 
     override fun doIitemClick() {
     }
@@ -24,5 +27,10 @@ class ListViewActivity : BaseActivity(), ListViewContract.View {
     }
 
     override fun initData() {
+        for(i in 0..10){
+            dataList.add("hello")
+        }
+        mAdapter = ListViewAdapter(dataList, this.baseContext)
+        list_view_lv?.adapter = mAdapter
     }
 }
